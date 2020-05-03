f1af2b1b681201624ff1124d2f1a336bf63c0ffd
Mark Egge <mark@eateggs.com>
2020-05-03 14:01:23 -0600

    switched to PennDOT Traffic shapefile

--- R/summary_plots.R
+++ R/summary_plots.R
@@ -7,27 +7,30 @@
 summary_plots <- function(df, out_file = "out/summary_plots.pdf") {
   if(out_file == "out/summary_plots.pdf")
     dir.create("out", showWarnings = FALSE)
+  suppressMessages({
+    pdf(file = out_file, paper = "letter", w = 8, h = 9)
+    par(mfrow =c (3,3))
+    par(mar = c(5.1, 4.1, 4.1, 2.1))
+    for(col in colnames(df)) {
+      if(is.character(df[[col]]) &&
+         length(na.omit(unique(df[[col]]))) > 0 &&
+         length(unique(df[[col]])) < 50) {
+        df[[col]] <- as.factor(df[[col]])
+      }
+      
+      if(is.factor(df[[col]])) {
+        tryCatch(barplot(table(df[[col]], exclude = c("", NA, NaN)), main=col),
+                 error = function(e) {
+                   print(paste("error plotting", col))
+                   NaN
+                 }
+        )
+      } else if (is.numeric(df[[col]])) {
+        hist(df[[col]], main=col)
+      }
+    }
+    dev.off()
+  })
   
-  pdf(file = out_file, paper = "letter", w = 8, h = 9)
-  par(mfrow =c (3,3))
-  par(mar = c(5.1, 4.1, 4.1, 2.1))
-  for(col in colnames(df)) {
-    if(is.character(df[[col]]) &&
-       length(na.omit(unique(df[[col]]))) > 0 &&
-       length(unique(df[[col]])) < 50) {
-      df[[col]] <- as.factor(df[[col]])
-    }
-    
-    if(is.factor(df[[col]])) {
-      tryCatch(barplot(table(df[[col]], exclude = c("", NA, NaN)), main=col),
-               error = function(e) {
-                 print(paste("error plotting", col))
-                 NaN
-               }
-      )
-    } else if (is.numeric(df[[col]])) {
-      hist(df[[col]], main=col)
-    }
-  }
-  dev.off()
+  cat("Plots generated to ", out_file, ".")
 }
